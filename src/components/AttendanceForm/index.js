efb797a64962dc3e614386121a94e8153961322b
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-24 13:49:45 +0000

    create methods to handle inputs and form submittion

--- src/components/AttendanceForm/index.js
+++ src/components/AttendanceForm/index.js
@@ -1,31 +1,72 @@
-import React, { Component } from 'react';
-import { Button, Form, Container, Header } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Button, Form, Container, Header } from "semantic-ui-react";
 
 class AttendanceForm extends Component {
+
+    constructor(props) {
+        super(props)
+      
+        this.state = {
+            attending: true,
+            name: "",
+            age: "",
+            salary: "",
+            hobby: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({ [e.target.name] : e.target.value} )
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
+
+        const { attending, name, adults, children } = this.state;
+
         return (
             <Container fluid className="container">
-                <Header as='h2'>React Google Sheets!</Header>
-                <Form className="form">
-                <Form.Field>
-                    <label>Name</label>
-                    <input placeholder='Enter your name' />
-                </Form.Field>
-                <Form.Field>
-                    <label>Age</label>
-                    <input placeholder='Enter your age' />
-                </Form.Field>
-                <Form.Field>
-                    <label>Salary</label>
-                    <input placeholder='Enter your salary' />
-                </Form.Field>
-                <Form.Field>
-                    <label>Hobby</label>
-                    <input placeholder='Enter your hobby' />
-                </Form.Field>
-                
-                <Button color="blue" type='submit'>Submit</Button>
-                </Form>
+                <Header as="h2">React Google Sheets!</Header>
+                    <Form className="form" onSubmit={ this.handleSubmit }>
+                        <Form.Field>
+                            <label>{"Attending the wedding?"}</label>
+                            <input placeholder="Attending the wedding?"
+                            name="attending"
+                            value={ attending }
+                            onChange={ this.handleChange }/>
+                        </Form.Field>
+
+                        <Form.Field>
+                            <label>{"Full Name"}</label>
+                            <input placeholder="Enter your name" name="name" value={ name } onChange={ this.handleChange }/>
+                        </Form.Field>
+
+                        <Form.Field>
+                            <label>{"Number of adults"}</label>
+                            <input
+                                placeholder="Enter your age"
+                                name="adults"
+                                value={ adults }
+                                onChange={ this.handleChange }/>
+                        </Form.Field>
+
+                        <Form.Field>
+                            <label>{"Number of children"}</label>
+                            <input placeholder="Enter your salary"
+                            name="children"
+                            value={ children }
+                            onChange={ this.handleChange }/>
+                        </Form.Field>
+
+                        <Button color="blue" type="submit">{"Submit"}</Button>
+                    </Form>
             </Container>
         )
     }
