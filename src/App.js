98d1be5a111940ef8963c190d38d4a88512bcfe7
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-25 09:39:37 +0000

    import react router dom into App and insert attendnace form into the home route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import AttendanceForm from "./components/AttendanceForm";
+import { HashRouter as Router, Route } from "react-router-dom";
+  
 
 function App() {
   return (
-    <AttendanceForm />
+    <Router>
+      <Route exact path="/">
+        <AttendanceForm />
+      </Route>
+    </Router>
   );
 }
 
